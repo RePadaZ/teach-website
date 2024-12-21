71d2a3166fa53f65b106ff7501dd902bb78daa56
padaz <Ne-PadaZ@yandex.ru>
2024-12-21 21:50:50 +0300

    add create React component TRCP

---
+++ frontend/src/lib/trcp-create.tsx
@@ -0,0 +1,8 @@
+import {createTRPCReact} from "@trpc/react-query";
+import type {TRPC_ROUTER} from "@ideanick/backend/src/trpc.ts";
+
+/*
+ Так как это отдельный команет React мы его создаем в отдельном файле
+ чтоб каждый компонет был в своем файле
+ */
+export const TRPC = createTRPCReact<TRPC_ROUTER>();
