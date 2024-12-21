da81fb86ccfd877171f375dfbe6e514b05fc588b
padaz <Ne-PadaZ@yandex.ru>
2024-12-21 16:33:14 +0300

    add test point

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+import express from "express";
+
+const expressApp = express();
+
+expressApp.get("/", (req: express.Request, res: express.Response) => {
+    res.send("Hello World!");
+});
+
+expressApp.listen(8080, () => {
+    console.log("Server started on port 8080");
+});
+
