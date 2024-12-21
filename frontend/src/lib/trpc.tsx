a2ab860037fbe31ba3246d5fda1fb67d9ac3dcc8
padaz <Ne-PadaZ@yandex.ru>
2024-12-21 21:26:51 +0300

    All packages combined

--- frontend/src/lib/trpc.tsx
+++ frontend/src/lib/trpc.tsx
@@ -1,6 +1,31 @@
-import type { TRPC_ROUTER } from "@ideanick/backend/src/trpc"
-import { createTRPCReact } from "@trpc/react-query";
+import type {TRPC_ROUTER} from "@ideanick/backend/src/trpc"
+import {createTRPCReact, httpBatchLink} from "@trpc/react-query";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import React from "react";
 
-const TRPC = createTRPCReact<TRPC_ROUTER>({
+const TRPC = createTRPCReact<TRPC_ROUTER>();
 
+const QUERY_CLIENT = new QueryClient({
+    defaultOptions: {
+        queries: {
+            retry: false,
+            refetchOnWindowFocus: false,
+        },
+    },
 })
+
+const TRPC_CLIENT = TRPC.createClient({
+    links: [
+        httpBatchLink({
+            url: "http://localhost:8080",
+        }),
+    ],
+})
+
+export const TRPC_PROVIDER = ({children} : {children: React.ReactNode}) => {
+    return (
+        <TRPC.Provider client={TRPC_CLIENT} queryClient={QUERY_CLIENT}>
+            <QueryClientProvider client={QUERY_CLIENT}>{children}</QueryClientProvider>
+        </TRPC.Provider>
+    )
+}
