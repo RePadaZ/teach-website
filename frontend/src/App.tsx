a3d55c13dbe12a895a9393c681904254b31ae947
RePadaZ <padazbze007@gmail.com>
2025-01-01 06:40:31 +0300

    add router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
 import {TRPC_PROVIDER} from "./lib/trpc.tsx";
 import {Header} from "./pages/template/Header.tsx";
 import {Cards} from "./pages/cards/Cards.tsx";
-import { Footer } from "./pages/template/Footer.tsx"
+import {Footer} from "./pages/template/Footer.tsx"
+import {Main} from "./pages/main/Main.tsx";
+import {Login} from "./pages/login/Login.tsx";
+import {Sing} from "./pages/sign/Sign.tsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import "./styles/global.scss"
 
 export const App = () => {
     return (
         <TRPC_PROVIDER>
             <Header />
-            <Cards />
+                <BrowserRouter>
+                    <Routes >
+                        <Route path="/" element={<Main/>} />
+                        <Route path="/cards" element={<Cards />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/sign" element={<Sing />} />
+                    </Routes>
+                </BrowserRouter>
             <Footer />
         </TRPC_PROVIDER>
     )
-} 
+}
