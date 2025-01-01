832e0da31f6554b105e3bde4195d951976e29e36
RePadaZ <padazbze007@gmail.com>
2025-01-01 21:35:28 +0300

    create first container

--- frontend/src/pages/main/Main.tsx
+++ frontend/src/pages/main/Main.tsx
@@ -1,24 +1,40 @@
-import {Container} from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import scss from './main.module.scss';
 
-export function Main(){
+export function Main() {
     return (
         <Container
             fluid
-            className={`d-flex justify-content-center align-items-center text-center ${scss.customBackStyle}`}
+            className={`p-0 ${scss.mainContainer}`}
         >
-            <div className=''>
-                <h1 className=''>
-                    Learn to Code
-                </h1>
-
-                <h3 className=''>
-                    You will find many useful resources here,
-                    <br/>including articles, lessons, code examples, and practical assignments.
-                </h3>
-
-            </div>
-
+            <Row className='g-0 h-100'>
+                {/* Фон в виде адаптивной картинки */}
+                <Col xs={12} className='position-relative p-0'>
+                    <Image
+                        src="mainBackGround.jpg" // Путь к изображению
+                        alt="Background"
+                        className='img-fluid w-100 h-100'
+                        style={{ objectFit: 'cover' }}
+                    />
+                    {/* Контент поверх фона */}
+                    <div className={`position-absolute top-50 start-50 translate-middle 
+                                     text-center text-white ${scss.contentWrapper}`}>
+                        <h1 className='fs-1 fw-bold'>
+                            Learn to Code
+                        </h1>
+                        <h3 className='fs-3 mb-4'>
+                            You will find many useful resources here,
+                            <br/>including articles, lessons, code examples, and practical assignments.
+                        </h3>
+                        <a
+                            href="/to_start"
+                            className={`fs-4 fw-bold ${scss.hoverEffect}`}
+                        >
+                            Not Sure Where To Begin?
+                        </a>
+                    </div>
+                </Col>
+            </Row>
         </Container>
-    )
+    );
 }
