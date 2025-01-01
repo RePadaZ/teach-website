62f435a95d901ab4c09e5d4dd92ab1e97c8d8825
RePadaZ <padazbze007@gmail.com>
2025-01-01 17:21:38 +0300

    create main

--- frontend/src/pages/main/Main.tsx
+++ frontend/src/pages/main/Main.tsx
@@ -1,9 +1,24 @@
+import {Container} from 'react-bootstrap';
+import scss from './main.module.scss';
 
 export function Main(){
     return (
-        <div className="app-container">
-            <h1>Заглушка</h1>
-            <p>Эта страница в разработке. Пожалуйста, зайдите позже!</p>
-        </div>
+        <Container
+            fluid
+            className={`d-flex justify-content-center align-items-center text-center ${scss.customBackStyle}`}
+        >
+            <div className=''>
+                <h1 className=''>
+                    Learn to Code
+                </h1>
+
+                <h3 className=''>
+                    You will find many useful resources here,
+                    <br/>including articles, lessons, code examples, and practical assignments.
+                </h3>
+
+            </div>
+
+        </Container>
     )
 }
