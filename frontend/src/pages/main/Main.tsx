3d5cbbefdef69c6b59a850915a93584d98b60a5c
RePadaZ <padazbze007@gmail.com>
2025-01-01 05:51:16 +0300

    add main page

---
+++ frontend/src/pages/main/Main.tsx
@@ -0,0 +1,9 @@
+
+export function Main(){
+    return (
+        <div className="app-container">
+            <h1>Заглушка</h1>
+            <p>Эта страница в разработке. Пожалуйста, зайдите позже!</p>
+        </div>
+    )
+}
