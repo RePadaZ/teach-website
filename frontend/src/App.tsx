0d2b7fa4f29d0fc5b52bf7e75dcea77a2e601317
RePadaZ <padazbze007@gmail.com>
2025-01-03 17:03:18 +0300

    add new component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import {TRPC_PROVIDER} from "./lib/trpc.tsx";
-import {Header} from "./pages/template/Header.tsx";
+import {Header} from "./pages/header/Header.tsx";
 import {Cards} from "./pages/cards/Cards.tsx";
-import {Footer} from "./pages/template/Footer.tsx"
+import {Footer} from './pages/footer/Footer.tsx'
 import {Main} from "./pages/main/Main.tsx";
 import {Login} from "./pages/login/Login.tsx";
 import {Sing} from "./pages/sign/Sign.tsx";
 import {ToStart} from "./pages/to_start/ToStart.tsx";
+import {Clock} from "./pages/tech_examples/Clock.tsx";
+import {ToDoList} from "./pages/tech_examples/ToDoList.tsx";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 export const App = () => {
@@ -19,6 +21,8 @@ export const App = () => {
                         <Route path="/login" element={<Login />} />
                         <Route path="/sign" element={<Sing />} />
                         <Route path="/to_start" element={<ToStart />} />
+                        <Route path="/cards/clock" element={<Clock />} />
+                        <Route path="/cards/todolist" element={<ToDoList />} />
                     </Routes>
                 </BrowserRouter>
             <Footer />
