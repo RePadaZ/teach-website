8ee3046eb6751dfd6a1bc28e97d8a68bd57ab4bd
RePadaZ <padazbze007@gmail.com>
2025-01-04 16:48:13 +0300

    style header

--- frontend/src/pages/header/Header.tsx
+++ frontend/src/pages/header/Header.tsx
@@ -1,5 +1,4 @@
-import {Navbar, Nav, NavDropdown, Button, Container, Dropdown} from 'react-bootstrap';
-import scss from './header.module.scss';
+import {Navbar, Nav, Button, Container, Dropdown} from 'react-bootstrap';
 
 export function Header() {
     return (
@@ -13,49 +12,70 @@ export function Header() {
 
                 {/* Navigation */}
                 <Nav className="mx-auto gap-3">
-                    <NavDropdown
-                        title="Frontend"
-                        id="frontend-dropdown"
-                        className={`btn-secondary bg-white text-dark ${scss.customButtonStyle}`}
-                    >
-                        <Dropdown.Item href={'/'}>Learn HTML</Dropdown.Item>
-                        <Dropdown.Item href={'/'}>Learn CSS</Dropdown.Item>
-                        <Dropdown.Item href={'/'}>Learn Bootstrap</Dropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown
-                        title="Backend"
-                        id="backend-dropdown"
-                        className={`btn-secondary bg-white text-dark ${scss.customButtonStyle}`}
-                    >
-                        <Dropdown.Item href={'/'}>Learn TypeScript</Dropdown.Item>
-                        <Dropdown.Item href={'/'}>Learn Java</Dropdown.Item>
-                        <Dropdown.Item href={'/'}>Learn Golang</Dropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown
-                        title="Game developer"
-                        id="frontend-dropdown"
-                        className={`btn-secondary bg-white text-dark ${scss.customButtonStyle}`}
-                    >
-                        <Dropdown.Item href={'/'}>Learn Rust</Dropdown.Item>
-                        <Dropdown.Item href={'/'}>Learn C#</Dropdown.Item>
-                        <Dropdown.Item href={'/'}>Learn C++</Dropdown.Item>
-                    </NavDropdown>
-                    <NavDropdown
-                        title="Services"
-                        id="services-dropdown"
-                        className={`btn-secondary bg-white text-dark ${scss.customButtonStyle}`}
-                    >
-                        <Dropdown.Item href={'/login'}>Login in / Sign Up</Dropdown.Item>
-                        <Dropdown.Item href={'/'}>Templates</Dropdown.Item>
-                        <Dropdown.Item href={'/'}>How to create</Dropdown.Item>
-                        <Dropdown.Item href={'/cards'}>Tech examples</Dropdown.Item>
-                    </NavDropdown>
+                    <Dropdown data-bs-theme="dark">
+                        <Dropdown.Toggle
+                            id="frontend-dropdown"
+                            variant="outline-info"
+                        >
+                            Frontend
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="/">Learn HTML</Dropdown.Item>
+                            <Dropdown.Item href="/">Learn CSS</Dropdown.Item>
+                            <Dropdown.Item href="/">Learn Bootstrap</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Dropdown data-bs-theme="dark">
+                        <Dropdown.Toggle
+                            id="backend-dropdown"
+                            variant="outline-info"
+                        >
+                            Backend
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="/">Learn TypeScript</Dropdown.Item>
+                            <Dropdown.Item href="/">Learn Java</Dropdown.Item>
+                            <Dropdown.Item href="/">Learn Golang</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Dropdown data-bs-theme="dark">
+                        <Dropdown.Toggle
+                            id="game-dev-dropdown"
+                            variant="outline-info"
+                        >
+                            Game Developer
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="/">Learn Rust</Dropdown.Item>
+                            <Dropdown.Item href="/">Learn C#</Dropdown.Item>
+                            <Dropdown.Item href="/">Learn C++</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Dropdown data-bs-theme="dark">
+                        <Dropdown.Toggle
+                            key='Info'
+                            id="services-dropdown"
+                            variant="outline-info"
+                        >
+                            Services
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="/login">Login / Sign Up</Dropdown.Item>
+                            <Dropdown.Item href="/">Templates</Dropdown.Item>
+                            <Dropdown.Item href="/">How to create</Dropdown.Item>
+                            <Dropdown.Item href="/cards">Tech Examples</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </Nav>
 
                 {/* Login Button */}
                 <Button
+                    className='me-5'
                     href={'/login'}
-                    className={`btn-secondary bg-white text-dark text-dark ${scss.customButtonStyle}`}
+                    variant="outline-info"
                 >
                     Login in / Sign Up
                 </Button>
