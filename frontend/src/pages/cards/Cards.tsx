16bad3b1235fe58b0e0858c23a638c14a6b1f06d
RePadaZ <padazbze007@gmail.com>
2025-01-06 07:11:25 +0300

    add array for cards

--- frontend/src/pages/cards/Cards.tsx
+++ frontend/src/pages/cards/Cards.tsx
@@ -1,57 +1,31 @@
-import { Container, Row, Col, Card } from 'react-bootstrap';
+import {Container, Row, Col, Card} from 'react-bootstrap';
 
 export function Cards() {
-    return (
-        <>
-            <Container className="mt-4 mb-4 vh-100">
-                <Row xs={1} md={4} className="g-4">
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src="https://via.placeholder.com/150" alt="Card Image" />
-                            <Card.Body>
-                                <Card.Title className="text-center">Clock</Card.Title>
-                                <Card.Text as="a" href={'/cards/clock'} className="text-center d-block">
-                                    Clock in JavaScript.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src="https://via.placeholder.com/150" alt="Card Image" />
-                            <Card.Body>
-                                <Card.Title className="text-center">To Do List</Card.Title>
-                                <Card.Text as="a" href={'/cards/todolist'} className="text-center d-block">
-                                    Simple To Do List in JavaScript
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src="https://via.placeholder.com/150" alt="Card Image" />
-                            <Card.Body>
-                                <Card.Title className="text-center">Карточка</Card.Title>
-                                <Card.Text as="a" href="#" className="text-center d-block">
-                                    Описание карточки.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col>
-                        <Card>
-                            <Card.Img variant="top" src="https://via.placeholder.com/150" alt="Card Image" />
-                            <Card.Body>
-                                <Card.Title className="text-center">Карточка</Card.Title>
-                                <Card.Text as="a" href="#" className="text-center d-block">
-                                    Описание карточки.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
 
-            </Container>
-        </>
+    const cards = [
+        { image: 'https://placehold.co/400', title: `Clock.`, href: '/cards/clock', text: 'Clock in JavaScript.'},
+        { image: 'https://placehold.co/400', title: `ToDoList.`, href: '/cards/todolist', text: 'ToDolist in JavaScript.'},
+        { image: 'https://placehold.co/400', title: `Clock.`, href: '/cards/clock', text: 'Clock in JavaScript.'},
+        { image: 'https://placehold.co/400', title: `Clock.`, href: '/cards/clock', text: 'Clock in JavaScript.'},
+    ];
+
+    return (
+        <Container fluid className='min-vh-100 py-5 GlobalBackGround'>
+            <Row xs={12} md={4} className=" m-5">
+                {cards.map((card) => (
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={card.image} alt="Card Image"/>
+                            <Card.Body>
+                                <Card.Title className="text-center">{card.title}</Card.Title>
+                                <Card.Text as="a" href={card.href} className="text-center d-block">
+                                    {card.text}
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))};
+            </Row>
+        </Container>
     );
 }
