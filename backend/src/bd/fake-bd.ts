1d4db5dad4740f817eae8c5a9bb35a10c5ec59a8
RePadaZ <padazbze007@gmail.com>
2025-01-21 21:46:12 +0300

    Creating a fake testing base

---
+++ backend/src/bd/fake-bd.ts
@@ -0,0 +1,41 @@
+export const cards= [
+    { id: 1, image: 'https://placehold.co/400', title: 'Clock.', href: '/tech-examples/clock', text: 'Clock in JavaScript.' },
+    { id: 2, image: 'https://placehold.co/400', title: 'ToDoList.', href: '/tech-examples/todolist', text: 'ToDo list in JavaScript.' },
+    { id: 3, image: 'https://placehold.co/400', title: 'Clock.', href: '/tech-examples/clock', text: 'Clock in JavaScript.' },
+    { id: 4, image: 'https://placehold.co/400', title: 'Clock.', href: '/tech-examples/clock', text: 'Clock in JavaScript.' },
+];
+
+export const header= [
+    {id: 1, "name": 'Frontend', "items": [
+            { "href": "/html", "label": "Learn HTML" },
+            { "href": "/css", "label": "Learn CSS" },
+            { "href": "/java-script", "label": "Learn JavaScript" },
+            { "href": "/bootstrap", "label": "Learn Bootstrap" }
+        ]
+    },
+    {id: 2, "name": 'Backend', "items": [
+            { "href": "/type-script", "label": "Learn TypeScript" },
+            { "href": "/java", "label": "Learn Java" },
+            { "href": "/golang", "label": "Learn Golang" }
+        ]
+    },
+    {id: 3, "name": 'Game Developer', "items": [
+            { "href": "/rust", "label": "Learn Rust" },
+            { "href": "/C#", "label": "Learn C#" },
+            { "href": "/C++", "label": "Learn C++" }
+        ]
+    },
+    {id: 4, "name": '1C Developer', "items": [
+            { "href": "/1c-base", "label": "Learn Base 1C" },
+            { "href": "/1c-form", "label": "Learn Form" },
+            { "href": "/1c-query", "label": "Learn Query" }
+        ]
+    },
+    {id: 5, "name": 'Services', "items": [
+            { "href": "/login", "label": "Login / Sign Up" },
+            { "href": "/templates", "label": "Templates" },
+            { "href": "/how-to-create", "label": "How to create" },
+            { "href": "/tech-examples", "label": "Tech Examples" },
+        ]
+    }
+]
