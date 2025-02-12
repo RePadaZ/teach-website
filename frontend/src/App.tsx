7cd94194fb1474ee1d12add918fdbb46e24c297c
RePadaZ <padazbze007@gmail.com>
2025-02-12 20:51:23 +0300

    Format import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import {TRPC_PROVIDER} from "./lib/trpc.tsx";
-import {Header} from "./pages/header/Header.tsx";
+import {Header} from "./components/header/Header.tsx";
 import {Cards} from "./pages/cards/Cards.tsx";
-import {Footer} from './pages/footer/Footer.tsx'
+import {Footer} from './components/footer/Footer.tsx'
 import {Main} from "./pages/main/Main.tsx";
 import {Login} from "./pages/login/Login.tsx";
-import {Sing} from "./pages/sign/Sign.tsx";
+import {Sign} from "./pages/sign/Sign.tsx";
 import {ToStart} from "./pages/to_start/ToStart.tsx";
 import {Clock} from "./pages/tech-examples/Clock.tsx";
 import {ToDoList} from "./pages/tech-examples/ToDoList.tsx";
@@ -16,21 +16,21 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 export const App = () => {
     return (
         <TRPC_PROVIDER>
-            {<Header />}
+            <Header/>
                 <BrowserRouter>
-                    <Routes >
-                        <Route path={'/'} element={<Main/>} />
-                        <Route path={'/tech-examples'} element={<Cards />} />
-                        <Route path={'/login'} element={<Login />} />
-                        <Route path={'/sign'} element={<Sing />} />
-                        <Route path={'/to-start'} element={<ToStart />} />
-                        <Route path={'/tech-examples/clock'} element={<Clock />} />
-                        <Route path={'/tech-examples/todolist'} element={<ToDoList />} />
-                        <Route path={'/contact'} element={<ContactForm />} />
-                        <Route path={'/1C-Base'} element={<OneCBase />} />
+                    <Routes>
+                        <Route path={'/'} element={<Main/>}/>
+                        <Route path={'/tech-examples'} element={<Cards/>}/>
+                        <Route path={'/login'} element={<Login/>}/>
+                        <Route path={'/sign'} element={<Sign/>}/>
+                        <Route path={'/to-start'} element={<ToStart/>}/>
+                        <Route path={'/tech-examples/clock'} element={<Clock/>}/>
+                        <Route path={'/tech-examples/todolist'} element={<ToDoList/>}/>
+                        <Route path={'/contact'} element={<ContactForm/>}/>
+                        <Route path={'/1C-Base'} element={<OneCBase/>}/>
                     </Routes>
                 </BrowserRouter>
-            <Footer />
+            <Footer/>
         </TRPC_PROVIDER>
     )
 }
