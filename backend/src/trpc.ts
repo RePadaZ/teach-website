a7a06756728a29f5861ecefad9558cdba149e559
RePadaZ <padazbze007@gmail.com>
2025-02-13 16:42:15 +0300

    Update TS and Formatting TRPC

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,23 +1,16 @@
 import {initTRPC} from "@trpc/server";
 import {cards, header, mainCards} from "./bd/fake-bd";
 
+
 // Создаем TRPC для связи типов между backend и frontend
-const TRPC = initTRPC.create();
+export const TRPC = initTRPC.create();
 
 // Создаем роуетер для наших запросов
 export const TRPC_ROUTER = TRPC.router({
 
-    getCards: TRPC.procedure.query(() => {
-        return {cards};
-    }),
-
-    getHeader: TRPC.procedure.query(() => {
-        return {header};
-    }),
-
-    getMainCards: TRPC.procedure.query(() => {
-        return {mainCards};
-    })
+    getCards: TRPC.procedure.query(() => ({ cards })),
+    getHeader: TRPC.procedure.query(() => ({ header })),
+    getMainCards: TRPC.procedure.query(() => ({ mainCards })),
 
 });
 
