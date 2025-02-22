b994bbb858e0a6286198a39983df2ed15304e7ae
RePadaZ <padazbze007@gmail.com>
2025-02-22 20:08:51 +0300

    Create ctx and void async

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import {TRPC_ROUTER} from "./trpc/router";
+import {AppContext, CreateAppContext} from "./ctx/ctx";
+
+
+// Обертка в async если для дожидания завершения процессов
+void (async () => {
+    // Создание контекста
+    let ctx: AppContext | null = null;
+    try {
+        // Присвоение контекста
+        ctx = CreateAppContext()
+
+        // Создаем приложения Express
+        const expressApp = express();
+
+        // Используем cors для того чтоб решить проблему запросов с клиета на наш сервер
+        expressApp.use(cors());
+
+        // Создаем endpoint для TRPC который будет обрабатывать все наши endpoint
+        expressApp.use("/", trpcExpress.createExpressMiddleware({
+            router: TRPC_ROUTER,
+        }))
+
+        // Включаем прослушку 8080 порта для нашего сервера
+        expressApp.listen(8080, () => {
+            console.log("Server started on port http://localhost:8080/");
+        });
+    } catch (err) {
+        console.log(err);
+        await ctx?.stop()
+    }
+
+})()
