e9f0a17da6fa8a4d77ab53cd4e83c3f73126a18b
RePadaZ <padazbze007@gmail.com>
2025-02-26 21:23:34 +0300

    Add code and syntax-highlighter

--- frontend/src/pages/tech-examples-pages/clock.tsx
+++ frontend/src/pages/tech-examples-pages/clock.tsx
@@ -1,5 +1,43 @@
 import {useEffect, useState} from "react";
 import {Transition} from "@headlessui/react";
+import {Light as SyntaxHighlighter} from 'react-syntax-highlighter';
+import ts from 'react-syntax-highlighter/dist/esm/languages/hljs/typescript';
+import styleCode from 'react-syntax-highlighter/dist/esm/styles/hljs/stackoverflow-dark';
+
+const CodePast = () => {
+    const codeString = '  // Состояние для времени\n' +
+        '    const [time, setTime] = useState(new Date());\n ' +
+        '// Настройка интервала\n' +
+        '    useEffect(() => {\n' +
+        '        const intervalId = setInterval(() => {\n' +
+        '            setTime(new Date());\n' +
+        '        }, 1000);\n' +
+        '\n' +
+        '        return () => clearInterval(intervalId);\n' +
+        '    }, []);\n' +
+        'return (\n' +
+        '<div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center py-12 px-6">\n' +
+        '   <Transition\n' +
+        '       key={time.toLocaleTimeString()} // Анимация при изменении времени\n' +
+        '       appear={true}\n' +
+        '       show={true}\n' +
+        '       enter="transition-opacity duration-500"\n' +
+        '       enterFrom="opacity-0"\n' +
+        '       enterTo="opacity-100"' +
+        '       >\n' +
+        '       <span className="text-6xl font-bold text-white mb-15">\n' +
+        '           {time.toLocaleTimeString()}\n' +
+        '       </span>\n' +
+        '   </Transition>\n' +
+        '</div>\n' +
+        ');';
+
+    return (
+        <SyntaxHighlighter language={ts} style={styleCode} lineNumbers={true}>
+            {codeString}
+        </SyntaxHighlighter>
+    );
+};
 
 export function Clock() {
     // Состояние для времени
@@ -20,7 +58,7 @@ export function Clock() {
             <div className="text-center text-white mb-8">
                 <h2 className="text-3xl font-bold mb-4">Часы на JavaScript</h2>
                 <p className="text-lg text-gray-300">
-                    Узнайте, как создать часы на чистом JavaScript и с использованием React.
+                    Узнайте, как создать часы на TypeScript и с использованием React.
                 </p>
             </div>
 
@@ -33,10 +71,11 @@ export function Clock() {
                 enterFrom="opacity-0"
                 enterTo="opacity-100"
             >
-    <span className="text-6xl font-bold text-white">
+    <span className="text-6xl font-bold text-white mb-15">
         {time.toLocaleTimeString()}
     </span>
             </Transition>
+            <CodePast/>
         </div>
     );
 }
