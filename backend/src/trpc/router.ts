03fd43a763a110792eda7ac9cdbc2441e363dbea
RePadaZ <padazbze007@gmail.com>
2025-03-10 00:51:46 +0300

    Add create and login user

--- backend/src/trpc/router.ts
+++ backend/src/trpc/router.ts
@@ -1,12 +1,14 @@
 import {TRPC} from "./init_trpc";
-import {CreateUserForm} from "../router-model/user_form";
+import {CreateUserForm} from "../router-model/create_user_form";
 import {CreateContactForm} from "../router-model/contact_form";
+import {LoginUserForm} from "../router-model/login_user_form";
 
 // Создаем роуетер для наших запросов
 export const TRPC_ROUTER = TRPC.router({
 
     CreateContactForm: CreateContactForm,
     CreateUserForm: CreateUserForm,
+    LoginUserForm: LoginUserForm,
 
 });
 
