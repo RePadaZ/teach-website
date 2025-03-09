ae37fe468f6025be1efd58e532c8276b695e2cc6
RePadaZ <padazbze007@gmail.com>
2025-03-10 01:26:55 +0300

    Renamed directory and add jwt

--- backend/src/trpc/router.ts
+++ backend/src/trpc/router.ts
@@ -1,7 +1,7 @@
 import {TRPC} from "./init_trpc";
-import {CreateUserForm} from "../router-model/create_user_form";
-import {CreateContactForm} from "../router-model/contact_form";
-import {LoginUserForm} from "../router-model/login_user_form";
+import {CreateUserForm} from "../router_model/create_user_form";
+import {CreateContactForm} from "../router_model/contact_form";
+import {LoginUserForm} from "../router_model/login_user_form";
 
 // Создаем роуетер для наших запросов
 export const TRPC_ROUTER = TRPC.router({
