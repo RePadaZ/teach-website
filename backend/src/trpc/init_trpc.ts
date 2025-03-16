d23c9fb9f9276dc695d00eb2735cb5ab4b1c4ee5
RePadaZ <padazbze007@gmail.com>
2025-03-16 18:38:48 +0300

    Add authorization

--- backend/src/trpc/init_trpc.ts
+++ backend/src/trpc/init_trpc.ts
@@ -1,5 +1,15 @@
-import {initTRPC} from "@trpc/server";
-import {AppContext} from "../ctx/ctx";
+import {inferAsyncReturnType, initTRPC} from "@trpc/server";
+import {type AppContext} from "../ctx/ctx";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import {ExpressRequest} from "../util_module/types";
+
+export const getCreateTrPC = (appContext: AppContext) =>
+    ({req}: trpcExpress.CreateExpressContextOptions) => ({
+        ...appContext,
+        me: (req as ExpressRequest).user || null,
+    })
+
+type TRPC_Context = inferAsyncReturnType<ReturnType<typeof getCreateTrPC>>;
 
 // Создаем TRPC для связи типов между backend и frontend
-export const TRPC = initTRPC.context<AppContext>().create();
+export const TRPC = initTRPC.context<TRPC_Context>().create();
