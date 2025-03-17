02573f2d705173657b3527cea699ef5db10920c8
RePadaZ <padazbze007@gmail.com>
2025-03-17 11:03:42 +0300

    Add salt password

--- backend/src/util_module/utils.ts
+++ backend/src/util_module/utils.ts
@@ -4,15 +4,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// Получение данных из env файла
 export const config = {
     JWT_SECRET: process.env.JWT_SECRET || "",
+    PASSWORD_SALT: process.env.PASSWORD_SALT || "",
 };
 
 // Функция для шифрования пароля
 export function cryptoPassword(password: string): string {
-    return crypto.createHash("md5").update(password).digest("hex");
+    return crypto.createHash("md5").update(`${config.PASSWORD_SALT}${password}`).digest("hex");
 }
 
+// Функция для генерация jwt токена
 export function SignJWT(userID: string): string {
     return jwt.sign(userID, config.JWT_SECRET);
 }
