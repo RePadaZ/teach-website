b72dad2361f73e1f73756d02bf01940246862eb1
RePadaZ <padazbze007@gmail.com>
2025-04-15 22:32:10 +0300

    Fix

--- frontend/src/pages/main/main.tsx
+++ frontend/src/pages/main/main.tsx
@@ -74,8 +74,8 @@ export function Main() {
                 <div className="w-full text-center">
                     <h2 className="text-4xl font-bold text-white mb-12">Разделы, доступные для обучения</h2>
                 </div>
-                {mainCards.map((card, key) => (
-                    <Cards key={key} title={card.title} text={card.text} href={card.href}
+                {mainCards.map((card, index) => (
+                    <Cards key={index} index={index} title={card.title} text={card.text} href={card.href}
                            button={card.button}/>
                 ))}
             </div>
